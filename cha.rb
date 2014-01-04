5944282ee2c85185dc3d41ab170de0e85b3cbe07
Phil Rigovanov <phil@rigovanov.ru>
2014-01-04 23:27:47 +0400

    well styled

--- cha.rb
+++ cha.rb
@@ -3,21 +3,12 @@ require 'taglib'
 
 Dir.chdir("/Volumes/phil/audiobooks/Bible")
 Dir["*.mp3"].each do |chapter|
-    # print chapter, "\t"
-    # if chapter != "rus_48_4.mp3" # && chapter =~ /rus_10_/)
-    	#print chapter, "\t"
-		TagLib::MPEG::File.open(chapter) do |file|
-
+	print chapter, "\t"
+	TagLib::MPEG::File.open(chapter) do |file|
 		tag = file.id3v2_tag
-        # puts tag.title
 		title = tag.frame_list('TIT2').first
-		# title.text = "Вторая книга Царств"
-		# title.text = "Вторая книга Царств" + title.to_s
-		title.text = title.to_s.gsub! ", ", ", глава "# { |match|  }
+		title.text = title.to_s.gsub! ", ", ", глава "
 		puts title, "\t"
-		# artist = tag.frame_list('TPE1').first
-		# puts artist # artist.text = "Библия"
 		file.save
-		end
-    # end
+	end
 end
--- list.rb
+++ list.rb
@@ -4,17 +4,11 @@ require 'taglib'
 Dir.chdir("/Volumes/phil/audiobooks/Bible")
 Dir["*.mp3"].each do |chapter|
     print chapter, "\t"
-    if chapter != "rus_48_4.mp3"
-		TagLib::MPEG::File.open(chapter) do |file|
-
+	TagLib::MPEG::File.open(chapter) do |file|
 		tag = file.id3v2_tag
-        # puts tag.title
 		title = tag.frame_list('TIT2').first
 		print title, "\t"
-		# title.text = "Бытие, глава 1"
 		artist = tag.frame_list('TPE1').first
-		puts artist # artist.text = "Библия"
-		# file.save
-		end
-    end
+		puts artist
+	end
 end
